2c2868da2996c134400acd39c222c6627340d9ff
Kaustav Das Modak <kaustav.dasmodak@freshworks.com>
2021-09-17 18:21:58 +0530

    Begin re-exporting models for freshteam
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@freshworks.com>

---
+++ src/freshteam/models.js
@@ -0,0 +1,12 @@
+/**
+ * @module models
+ */
+
+import { Address, Applicant, ApplicantCandidate, ApplicantConversation } from "../gen/freshteam";
+
+export default {
+  Address,
+  Applicant,
+  ApplicantCandidate,
+  ApplicantConversation
+};
