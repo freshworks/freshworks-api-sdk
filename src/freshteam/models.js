82b35a7065de9f8477674872637d9bf74f87eb22
Kaustav Das Modak <kaustav.dasmodak@freshworks.com>
2021-09-29 09:58:45 +0530

    Set types and interface for applicant API
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@freshworks.com>

--- src/freshteam/models.js
+++ src/freshteam/models.js
@@ -1,1 +1,14 @@
-export { Employee, EmployeeCreate, EmployeeDetail, InlineObject2 as EmployeeFieldCreate } from "../gen/freshteam";
+export {
+  // Applicant models
+  Applicant,
+  ApplicantDetail,
+  ApplicantUpdate,
+  InlineObject3 as ApplicantArchive,
+  InlineObject4 as ApplicantSubStage,
+
+  // Employee models
+  Employee,
+  EmployeeCreate,
+  EmployeeDetail,
+  InlineObject2 as EmployeeFieldCreate
+} from "../gen/freshteam";
