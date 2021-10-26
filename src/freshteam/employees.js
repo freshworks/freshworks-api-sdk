529d1261ce9a8721b3bf09ca783e63c40f353f39
Kaustav Das Modak <kaustav.dasmodak@freshworks.com>
2021-10-26 19:13:09 +0530

    Switch to typedoc. Switch to classes
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@freshworks.com>

--- src/freshteam/employees.js
+++ src/freshteam/employees.js
@@ -1,79 +1,80 @@
-/**
- * @module Freshteam/employees
- */
-
 import { EmployeeApi, EmployeeFieldsApi } from "../gen/freshteam";
 import { Client } from "../http-client";
 import { Employee, EmployeeCreate, EmployeeDetail, EmployeeField } from "./models";
 
-/**
- * @param {Client} client
- * @hideconstructor
- */
-export default function employees(client) {
-  let api = new EmployeeApi(client.instance);
-  let fieldsApi = new EmployeeFieldsApi(client.instance);
-
-  return {
+export class Employees {
+  /**
+   * @param {Client} client
+   * @hidden
+   */
+  constructor(client) {
     /**
-     * List all employees
-     *
-     * @param {object} [query = {}] - Query parameters
-     * @returns {Promise<Employee[]>} - Response with array of employees in the response body
+     * @private
      */
-    async list(query = {}) {
-      return api.getEmployees(query);
-    },
-
+    this._api = new EmployeeApi(client.instance);
     /**
-     * Find the details of an employee by ID
-     *
-     * @param {number} id - Employee ID
-     * @param {object} [query = {}] - Query parameters
-     * @returns {Promise<EmployeeDetail>} - Response with an employee object in the response body, if found
+     * @private
      */
-    async get(id, query = {}) {
-      return api.getEmployee(id, query);
-    },
+    this._fieldsApi = new EmployeeFieldsApi(client.instance);
+  }
+  /**
+   * List all employees
+   *
+   * @public
+   * @param {object} [query = {}] - Query parameters
+   * @returns {Promise<Employee[]>} - Response with array of employees in the response body
+   */
+  async list(query = {}) {
+    return this._api.getEmployees(query);
+  }
 
-    /**
-     * Create an employee
-     *
-     * @param {EmployeeCreate} employee - An Employee object
-     * @returns {Promise<Employee>} - Response with create employee object in the response body
-     */
-    async create(employee) {
-      return api.createEmployee(employee);
-    },
+  /**
+   * Find the details of an employee by ID
+   *
+   * @param {number} id - Employee ID
+   * @param {object} [query = {}] - Query parameters
+   * @returns {Promise<EmployeeDetail>} - Response with an employee object in the response body, if found
+   */
+  async get(id, query = {}) {
+    return this._api.getEmployee(id, query);
+  }
 
-    /**
-     * Update an existing employee data
-     *
-     * @param {number} id - Employee ID
-     * @param {Employee} employee - Employee to update
-     * @returns {Promise<Employee>} - Response with updated employee object in the response body, if found
-     */
-    async update(id, employee) {
-      return api.updateEmployee(id, employee);
-    },
+  /**
+   * Create an employee
+   *
+   * @param {EmployeeCreate} employee - An Employee object
+   * @returns {Promise<Employee>} - Response with create employee object in the response body
+   */
+  async create(employee) {
+    return this._api.createEmployee(employee);
+  }
+  /**
+   * Update an existing employee data
+   *
+   * @param {number} id - Employee ID
+   * @param {Employee} employee - Employee to update
+   * @returns {Promise<Employee>} - Response with updated employee object in the response body, if found
+   */
+  async update(id, employee) {
+    return this._api.updateEmployee(id, employee);
+  }
 
-    /**
-     * List all employee fields
-     *
-     * @returns {Promise<EmployeeField[]>} - Response with list of employee fields
-     */
-    async fields() {
-      return fieldsApi.getEmployeeFields();
-    },
+  /**
+   * List all employee fields
+   *
+   * @returns {Promise<EmployeeField[]>} - Response with list of employee fields
+   */
+  async fields() {
+    return this._fieldsApi.getEmployeeFields();
+  }
 
-    /**
-     * Create employee field
-     *
-     * @param {EmployeeFieldCreate} field - Request body
-     * @returns {Promise<EmployeeField>}
-     */
-    async createField(field) {
-      return fieldsApi.createEmployeeFiled(field); // It's a typo in the spec
-    }
-  };
+  /**
+   * Create employee field
+   *
+   * @param {EmployeeFieldCreate} field - Request body
+   * @returns {Promise<EmployeeField>}
+   */
+  async createField(field) {
+    return this._fieldsApi.createEmployeeFiled(field); // It's a typo in the spec
+  }
 }
--- src/freshteam/new-hires.js
+++ src/freshteam/new-hires.js
@@ -1,49 +1,48 @@
-/**
- * @module Freshteam/newHires
- */
-
 import { NewHireApi } from "../gen/freshteam";
 import { Client } from "../http-client";
 import { NewHire, NewHireCreate, NewHireDetail } from "./models";
 
-/**
- * @param {Client} client
- * @hideconstructor
- */
-export default function newHires(client) {
-  const api = new NewHireApi(client.instance);
-
-  return {
+export class NewHires {
+  /**
+   *
+   * @param {Client} client
+   * @hidden
+   */
+  constructor(client) {
     /**
-     * Creates a NewHire
-     *
-     * @param {NewHireCreate} newHire - Details of a NewHire
-     * @returns {Promise<NewHire>} - New Hire object
+     * @private
      */
-    async create(newHire) {
-      return api.createNewHire(newHire);
-    },
+    this._api = new NewHireApi(client.instance);
+  }
+  /**
+   * Creates a NewHire
+   *
+   * @param {NewHireCreate} newHire - Details of a NewHire
+   * @returns {Promise<NewHire>} - New Hire object
+   */
+  async create(newHire) {
+    return this._api.createNewHire(newHire);
+  }
 
-    /**
-     * Gets the details of a NewHire
-     *
-     * @param {number} id - Identifier of the NewHire
-     * @param {object} options - Additional options to add include properties
-     * @returns {Promise<NewHireDetail>} - Response with NewHire object in the response body
-     */
-    async get(id, options) {
-      return api.getNewHire(id, options);
-    },
+  /**
+   * Gets the details of a NewHire
+   *
+   * @param {number} id - Identifier of the NewHire
+   * @param {object} options - Additional options to add include properties
+   * @returns {Promise<NewHireDetail>} - Response with NewHire object in the response body
+   */
+  async get(id, options) {
+    return this._api.getNewHire(id, options);
+  }
 
-    /**
-     * Update a NewHire
-     *
-     * @param {number} id - Identifier of the NewHire
-     * @param {NewHireCreate} newHire - NewHire details
-     * @returns {Promise<NewHire>} - NewHire object
-     */
-    async update(id, newHire) {
-      return api.updateNewHire(newHire, id);
-    }
-  };
+  /**
+   * Update a NewHire
+   *
+   * @param {number} id - Identifier of the NewHire
+   * @param {NewHireCreate} newHire - NewHire details
+   * @returns {Promise<NewHire>} - NewHire object
+   */
+  async update(id, newHire) {
+    return this._api.updateNewHire(newHire, id);
+  }
 }
