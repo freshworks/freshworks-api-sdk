d2e868c0ee96fab6b6fd8f12778ddeef9d821de6
Kaustav Das Modak <kaustav.dasmodak@freshworks.com>
2021-11-02 12:02:20 +0530

    Add test for employees.list()
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@freshworks.com>

---
+++ test/freshteam/responses/get-employees-200.js
@@ -0,0 +1,201 @@
+export default [
+  {
+      "id": 6000207819,
+      "created_at": "2021-10-27T14:10:46.000Z",
+      "updated_at": "2021-10-27T14:10:46.000Z",
+      "deleted": false,
+      "work_numbers": null,
+      "nick_name": null,
+      "date_of_birth": null,
+      "gender": null,
+      "address": {},
+      "phone_numbers": [],
+      "communication_address": {},
+      "designation": null,
+      "joining_date": null,
+      "branch_id": 6000024809,
+      "team_id": null,
+      "business_unit_id": null,
+      "sub_department_id": null,
+      "department_id": null,
+      "reporting_to_id": null,
+      "hr_incharge_id": null,
+      "shift_id": null,
+      "termination_date": null,
+      "first_name": "Arthur",
+      "middle_name": null,
+      "last_name": "Dent",
+      "employee_id": null,
+      "status": "active",
+      "official_email": "dent1635343844702@example.com",
+      "personal_email": null,
+      "user_category": "regular",
+      "employee_type": "full_time",
+      "draft": false,
+      "terminated": false
+  },
+  {
+      "id": 6000122460,
+      "created_at": "2021-07-16T17:43:52.000Z",
+      "updated_at": "2021-07-19T07:45:31.000Z",
+      "deleted": false,
+      "work_numbers": [
+          {
+              "name": "Work",
+              "number": "(416)022-3562"
+          },
+          {
+              "name": "Others",
+              "number": "(416)099-8899"
+          }
+      ],
+      "nick_name": "Batman",
+      "date_of_birth": "1978-07-19",
+      "gender": "female",
+      "address": {
+          "street": "1312 Pape Avenue",
+          "state": "Ontario",
+          "city": "Toronto",
+          "country_code": "CA",
+          "zip_code": ""
+      },
+      "phone_numbers": null,
+      "communication_address": {
+          "communication_street": "1312 Pape Avenue",
+          "communication_state": "Ontario",
+          "communication_city": "Toronto",
+          "communication_country_code": "CA",
+          "communication_zip_code": ""
+      },
+      "designation": "Regional Manager",
+      "joining_date": "2021-07-16",
+      "branch_id": 6000024810,
+      "team_id": 6000117756,
+      "business_unit_id": null,
+      "sub_department_id": null,
+      "department_id": 6000060444,
+      "reporting_to_id": 6000207819,
+      "hr_incharge_id": 6000122462,
+      "shift_id": null,
+      "termination_date": null,
+      "first_name": "Corina",
+      "middle_name": null,
+      "last_name": "Willie (sample)",
+      "employee_id": "E003",
+      "status": "active",
+      "official_email": "cwillie@freshteam.com",
+      "personal_email": null,
+      "user_category": "regular",
+      "employee_type": "full_time",
+      "draft": false,
+      "terminated": false
+  },
+  {
+      "id": 6000122461,
+      "created_at": "2021-07-16T17:43:52.000Z",
+      "updated_at": "2021-07-19T07:45:30.000Z",
+      "deleted": false,
+      "work_numbers": [
+          {
+              "name": "Work",
+              "number": "(416)022-3562"
+          },
+          {
+              "name": "Others",
+              "number": "(416)099-8899"
+          }
+      ],
+      "nick_name": "Batman",
+      "date_of_birth": "1978-07-16",
+      "gender": "female",
+      "address": {
+          "street": "1312 Pape Avenue",
+          "state": "Ontario",
+          "city": "Toronto",
+          "country_code": "CA",
+          "zip_code": ""
+      },
+      "phone_numbers": null,
+      "communication_address": {
+          "communication_street": "1312 Pape Avenue",
+          "communication_state": "Ontario",
+          "communication_city": "Toronto",
+          "communication_country_code": "CA",
+          "communication_zip_code": ""
+      },
+      "designation": "Regional Manager",
+      "joining_date": "2021-07-16",
+      "branch_id": 6000024810,
+      "team_id": 6000117756,
+      "business_unit_id": null,
+      "sub_department_id": null,
+      "department_id": 6000060444,
+      "reporting_to_id": 6000207819,
+      "hr_incharge_id": 6000122462,
+      "shift_id": null,
+      "termination_date": null,
+      "first_name": "Dreda",
+      "middle_name": null,
+      "last_name": "Mikki (sample)",
+      "employee_id": "E004",
+      "status": "active",
+      "official_email": "dmikki@freshteam.com",
+      "personal_email": null,
+      "user_category": "regular",
+      "employee_type": "full_time",
+      "draft": false,
+      "terminated": false
+  },
+  {
+      "id": 6000122463,
+      "created_at": "2021-07-16T17:43:53.000Z",
+      "updated_at": "2021-07-19T07:45:31.000Z",
+      "deleted": false,
+      "work_numbers": [
+          {
+              "name": "Work",
+              "number": "(453)533-2234"
+          }
+      ],
+      "nick_name": "Spidey",
+      "date_of_birth": "1980-07-23",
+      "gender": "male",
+      "address": {
+          "street": "3221 St George Street",
+          "state": "British Columbia",
+          "city": "Vancouver",
+          "country_code": "CA",
+          "zip_code": ""
+      },
+      "phone_numbers": null,
+      "communication_address": {
+          "communication_street": "3221 St George Street",
+          "communication_state": "British Columbia",
+          "communication_city": "Vancouver",
+          "communication_country_code": "CA",
+          "zip_code": ""
+      },
+      "designation": "Sales Representative",
+      "joining_date": "2021-07-16",
+      "branch_id": 6000024810,
+      "team_id": 6000117760,
+      "business_unit_id": null,
+      "sub_department_id": null,
+      "department_id": 6000060444,
+      "reporting_to_id": 6000122459,
+      "hr_incharge_id": null,
+      "shift_id": null,
+      "termination_date": null,
+      "first_name": "Peter",
+      "middle_name": null,
+      "last_name": "Parker",
+      "employee_id": "E006",
+      "status": "active",
+      "official_email": "ppeter@freshteam.com",
+      "personal_email": null,
+      "user_category": "regular",
+      "employee_type": "full_time",
+      "draft": false,
+      "terminated": false
+  }
+]
