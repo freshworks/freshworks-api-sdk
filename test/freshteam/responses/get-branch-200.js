69756df91ce632edbfaf79c2a7c5bbe5c3f4575a
Kaustav Das Modak <kaustav.dasmodak@freshworks.com>
2021-12-01 11:24:23 +0530

    Prettier format
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@freshworks.com>

--- test/freshteam/responses/get-branch-200.js
+++ test/freshteam/responses/get-branch-200.js
@@ -1,17 +1,17 @@
 export default {
-    "id": 6000024810,
-    "created_at": "2021-07-16T17:43:47.000Z",
-    "updated_at": "2021-07-19T07:43:24.000Z",
-    "deleted": false,
-    "name": "Branch office",
-    "state": null,
-    "city": "San Francisco",
-    "country_code": "US",
-    "zip": null,
-    "time_zone": "Asia/Calcutta",
-    "currency": "USD",
-    "language": "en",
-    "main_office": false,
-    "date_format": "MonthFirst",
-    "street": null
-}
+  id: 6000024810,
+  created_at: "2021-07-16T17:43:47.000Z",
+  updated_at: "2021-07-19T07:43:24.000Z",
+  deleted: false,
+  name: "Branch office",
+  state: null,
+  city: "San Francisco",
+  country_code: "US",
+  zip: null,
+  time_zone: "Asia/Calcutta",
+  currency: "USD",
+  language: "en",
+  main_office: false,
+  date_format: "MonthFirst",
+  street: null
+};
