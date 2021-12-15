6115bf932442276cde61169d5954fae157c4050d
teja-kummarikuntla <teja.kummarikuntla@freshworks.com>
2021-12-15 16:56:53 +0530

    Add: tests:freshteam/employees

---
+++ test/freshteam/responses/create-employee-field-201.js
@@ -0,0 +1,13 @@
+export default {
+    "id": 6003487313,
+    "created_at": "2021-12-15T10:47:33.000Z",
+    "updated_at": "2021-12-15T10:47:33.000Z",
+    "employee_form_id": 6000011682,
+    "name": "hobbies_cf_str01",
+    "label": "Hobbies",
+    "field_type": "text",
+    "position": 6,
+    "default": false,
+    "section_label": "Personal Information",
+    "required": false
+}
---
+++ test/freshteam/responses/update-employee-200.js
@@ -0,0 +1,56 @@
+export default {
+  id: 6000122460,
+  created_at: "2021-07-16T17:43:52.000Z",
+  updated_at: "2021-12-15T11:05:44.000Z",
+  deleted: false,
+  work_numbers: [
+    {
+      name: "Work",
+      number: "(416)022-3562"
+    },
+    {
+      name: "Others",
+      number: "(416)099-8899"
+    }
+  ],
+  nick_name: "Batman",
+  date_of_birth: "1978-07-19",
+  gender: "female",
+  address: {
+    street: "1312 Pape Avenue",
+    state: "Ontario",
+    city: "Toronto",
+    country_code: "CA",
+    zip_code: ""
+  },
+  phone_numbers: null,
+  communication_address: {
+    communication_street: "1312 Pape Avenue",
+    communication_state: "Ontario",
+    communication_city: "Toronto",
+    communication_country_code: "CA",
+    communication_zip_code: ""
+  },
+  designation: "Regional Manager",
+  joining_date: "2021-07-16",
+  branch_id: 6000024810,
+  team_id: 6000117756,
+  business_unit_id: null,
+  sub_department_id: null,
+  department_id: 6000060444,
+  reporting_to_id: 6000122458,
+  hr_incharge_id: 6000122462,
+  shift_id: null,
+  termination_date: null,
+  first_name: "Johnny",
+  middle_name: "L",
+  last_name: "Snow",
+  employee_id: "E003",
+  status: "active",
+  official_email: "johnny@freshteam.com",
+  personal_email: null,
+  user_category: "regular",
+  employee_type: "full_time",
+  draft: false,
+  terminated: false
+};
