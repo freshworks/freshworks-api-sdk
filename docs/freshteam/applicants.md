b4e180aaab998fed545b5f5e7ad4015202f1fc37
Thakur Ganeshsingh Thakur <thakur.ganeshsingh@freshworks.com>
2022-11-14 13:32:57 +0530

    Add pagination header support to applicants in Freshteam API
    - Update applicant.js
    - Add test cases
    - update markdown docs

--- docs/freshteam/applicants.md
+++ docs/freshteam/applicants.md
@@ -11,40 +11,44 @@ next: /freshteam/branches
 const applicants = await ft.applicants.list(jobPostingId, query);
 ```
 
-- Returns a `Promise` that resolves to an `Array` of `Freshteam.models.ApplicantDetail` objects
+- Returns a `Response` object that resolves to an `Array` of `Freshteam.models.ApplicantDetail` objects
 - The second argument provides queries accepted by the REST API. (TODO: Document the query parameters)
 
 ## Get applicant by ID
 
 ```js
 const applicant = await ft.applicants.get(id);
+// use applicant.json() to access objects
 ```
 
-- Returns a `Promise` that resolves to a `Freshteam.models.ApplicantDetail` object
+- Returns a `Response` object that resolves to a `Freshteam.models.ApplicantDetail` object
 
 ## Update an applicant
 
 ```js
 const applicant = await ft.applicants.update(id, applicant);
+// use applicant.json() to access objects
 ```
 
-- Returns a `Promise` that resolves to a `Freshteam.models.ApplicantDetail` object
+- Returns a `Response` object that resolves to a `Freshteam.models.ApplicantDetail` object
 - The second argument is of type `Freshteam.models.ApplicantUpdate`
 
 ## Archive an applicant
 
 ```js
 const applicant = await ft.applicants.archive(id, options);
+// use applicant.json() to access objects
 ```
 
-- Returns a `Promise` that resolves to a `Freshteam.models.ApplicantDetail` object
+- Returns a `Response` object that resolves to a `Freshteam.models.ApplicantDetail` object
 - The second argument is of type `Freshteam.models.ApplicantArchive`
 
 ## Update applicant sub-stage
 
 ```js
 const applicant = await ft.applicants.updateSubStage(id, options);
+// use applicant.json() to access objects
 ```
 
-- Returns a `Promise` that resolves to a `Freshteam.models.ApplicantDetail` object
+- Returns a `Response` object that resolves to a `Freshteam.models.ApplicantDetail` object
 - The second argument is of type `Freshteam.models.ApplicantSubStage`
--- test/freshteam/applicants.test.js
+++ test/freshteam/applicants.test.js
@@ -1,5 +1,7 @@
 import { Freshteam } from "../../src";
 import nock from "nock";
+import { Response } from "../../src/http-client";
+import { Applicant, ApplicantDetail } from "../../src/gen/freshteam";
 
 const FT_DOMAIN = "example.freshteam.com";
 const FT_API_KEY = "f_xkcd_222";
@@ -15,21 +17,24 @@ describe("Applicants API", function () {
       const page = 2;
       mock.get(`/api/job_postings/${jobId}/applicants?page=${page}`).reply(200, []);
       const applicantsList = await ft.applicants.list(jobId, { page });
-      expect(applicantsList).toBeInstanceOf(Array);
+      expect(applicantsList).toBeInstanceOf(Response);
+      expect(applicantsList.json()).toBeInstanceOf(Array);
     });
 
     it("should support `sort` query param", async function () {
       const sort = "title";
       mock.get(`/api/job_postings/${jobId}/applicants?sort=${sort}`).reply(200, []);
       const applicantsList = await ft.applicants.list(jobId, { sort });
-      expect(applicantsList).toBeInstanceOf(Array);
+      expect(applicantsList).toBeInstanceOf(Response);
+      expect(applicantsList.json()).toBeInstanceOf(Array);
     });
 
     it("should support `sort_type` query param", async function () {
       const sort_type = "desc";
       mock.get(`/api/job_postings/${jobId}/applicants?sort_type=${sort_type}`).reply(200, []);
       const applicantsList = await ft.applicants.list(jobId, { sort_type });
-      expect(applicantsList).toBeInstanceOf(Array);
+      expect(applicantsList).toBeInstanceOf(Response);
+      expect(applicantsList.json()).toBeInstanceOf(Array);
     });
 
     it("should support all pagination query params when supplied together", async function () {
@@ -38,7 +43,19 @@ describe("Applicants API", function () {
       const sort_type = "desc";
       mock.get(`/api/job_postings/${jobId}/applicants?sort=${sort}&sort_type=${sort_type}&page=${page}`).reply(200, []);
       const applicantsList = await ft.applicants.list(jobId, { page, sort, sort_type });
-      expect(applicantsList).toBeInstanceOf(Array);
+      expect(applicantsList).toBeInstanceOf(Response);
+      expect(applicantsList.json()).toBeInstanceOf(Array);
     });
   });
+
+  describe("applicant CRUD suite", function () {
+    it("GET test for applicant : /api/applicants/{id}", async function () {
+      const applicantId = 2;
+      mock.get(`/api/applicants/${applicantId}`).reply(200, []);
+      const applicantsList = await ft.applicants.get(applicantId);
+      expect(applicantsList).toBeInstanceOf(Response);
+      expect(applicantsList.json()).toBeInstanceOf(ApplicantDetail);
+    });
+  });
+
 });
